0d351fea127a798a9c6ff92c69475dc02626c98b
jladera <38166729+jladera@users.noreply.github.com>
2020-05-19 20:36:41 -0700

    Added result page (not designed or functional)

---
+++ Is This Vegan/Is This Vegan/Is This Vegan/Views/Result.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Is_This_Vegan.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class Result : ContentPage
+{
+    public Result()
+    {
+        InitializeComponent();
+    }
+}
+}
