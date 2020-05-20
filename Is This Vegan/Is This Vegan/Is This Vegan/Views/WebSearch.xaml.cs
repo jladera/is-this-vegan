e7edcc726d84dc1b7ccbef6b38850db30030c90a
jladera <38166729+jladera@users.noreply.github.com>
2020-05-19 20:39:51 -0700

    Added web search page (not designed or functional)
    This will be how the user "google's" if an ingredient is vegan or not from within our app.

---
+++ Is This Vegan/Is This Vegan/Is This Vegan/Views/WebSearch.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Is_This_Vegan.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class WebSearch : ContentPage
+{
+    public WebSearch()
+    {
+        InitializeComponent();
+    }
+}
+}
