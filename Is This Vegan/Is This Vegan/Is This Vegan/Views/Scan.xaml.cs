76546d84a99025dfc72d0dfd3b55ba9ef72ebf64
jladera <38166729+jladera@users.noreply.github.com>
2020-05-21 17:22:38 -0700

    Updated temporary camera view's code-behind
    Can take, save, and access photos, now just need to style to be cohesive with our app design, remove unnecesary functionality, and use data-binding commands rather than an event handler.

--- Is This Vegan/Is This Vegan/Is This Vegan/Views/Scan.xaml.cs
+++ Is This Vegan/Is This Vegan/Is This Vegan/Views/Scan.xaml.cs
@@ -1,20 +1,202 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Code provided by Xam.Plugin.Media which can be found here:
+ * 
+ * https://github.com/jamesmontemagno/MediaPlugin
+ */
 
+using System;
+using Plugin.Media;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Is_This_Vegan.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class Scan : ContentPage
-{
-    public Scan()
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Scan : ContentPage
     {
-        InitializeComponent();
+        public Scan()
+        {
+            InitializeComponent();
+            //takePhoto.Clicked += async (sender, args) =>
+            //{
+
+            //    if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            //    {
+            //        await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+            //        return;
+            //    }
+
+            //    var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            //    {
+            //        PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+            //        Directory = "Sample",
+            //        Name = "test.jpg"
+            //    });
+
+            //    if (file == null)
+            //        return;
+
+            //    await DisplayAlert("File Location", file.Path, "OK");
+
+            //    image.Source = ImageSource.FromStream(() =>
+            //    {
+            //        var stream = file.GetStream();
+            //        file.Dispose();
+            //        return stream;
+            //    });
+            //};
+
+            //pickPhoto.Clicked += async (sender, args) =>
+            //{
+            //    if (!CrossMedia.Current.IsPickPhotoSupported)
+            //    {
+            //        await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+            //        return;
+            //    }
+            //    var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+            //    {
+            //        PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+            //    });
+
+
+            //    if (file == null)
+            //        return;
+
+            //    image.Source = ImageSource.FromStream(() =>
+            //    {
+            //        var stream = file.GetStream();
+            //        file.Dispose();
+            //        return stream;
+            //    });
+            //};
+
+            //takeVideo.Clicked += async (sender, args) =>
+            //{
+            //    if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakeVideoSupported)
+            //    {
+            //        await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+            //        return;
+            //    }
+
+            //    var file = await CrossMedia.Current.TakeVideoAsync(new Plugin.Media.Abstractions.StoreVideoOptions
+            //    {
+            //        Name = "video.mp4",
+            //        Directory = "DefaultVideos",
+            //    });
+
+            //    if (file == null)
+            //        return;
+
+            //    await DisplayAlert("Video Recorded", "Location: " + file.Path, "OK");
+
+            //    file.Dispose();
+            //};
+
+            //pickVideo.Clicked += async (sender, args) =>
+            //{
+            //    if (!CrossMedia.Current.IsPickVideoSupported)
+            //    {
+            //        await DisplayAlert("Videos Not Supported", ":( Permission not granted to videos.", "OK");
+            //        return;
+            //    }
+            //    var file = await CrossMedia.Current.PickVideoAsync();
+
+            //    if (file == null)
+            //        return;
+
+            //    await DisplayAlert("Video Selected", "Location: " + file.Path, "OK");
+            //    file.Dispose();
+            //};
+        }
+
+        public async void TakeAPhotoButton_OnClicked(Object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        public async void PickAPhotoButton_OnClicked(Object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+                return;
+            }
+            var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+            });
+
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        public async void TakeAVideoButton_OnClicked(Object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakeVideoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakeVideoAsync(new Plugin.Media.Abstractions.StoreVideoOptions
+            {
+                Name = "video.mp4",
+                Directory = "DefaultVideos",
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("Video Recorded", "Location: " + file.Path, "OK");
+
+            file.Dispose();
+        }
+
+        public async void PickAVideoButton_OnClicked(Object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsPickVideoSupported)
+            {
+                await DisplayAlert("Videos Not Supported", ":( Permission not granted to videos.", "OK");
+                return;
+            }
+            var file = await CrossMedia.Current.PickVideoAsync();
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("Video Selected", "Location: " + file.Path, "OK");
+            file.Dispose();
+        }
     }
-}
 }
