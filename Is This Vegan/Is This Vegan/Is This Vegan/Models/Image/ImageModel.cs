ccdadc836f3b649cd6ffeea1323cd467a9f8c7bb
jladera <38166729+jladera@users.noreply.github.com>
2020-05-24 20:24:51 -0700

    Added ImageModel class

---
+++ Is This Vegan/Is This Vegan/Is This Vegan/Models/Image/ImageModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Is_This_Vegan.Models.Image
+{
+    class ImageModel
+    {
+        public ImageSource Image { get; set; }
+
+        private string testImage = "Is This Vegan.SharedMedia.test_ingredient_list_full.jpg";
+
+        /// <summary>
+        /// Creates an ImageModel. If resourceImagePath is null or empty, assume we are testing and so use
+        /// pre-existing test image
+        /// </summary>
+        /// <param name="resourceImagePath"> Path to an EmbeddedResource image </param>
+        public ImageModel(string resourceImagePath = null)
+        {
+            if (string.IsNullOrEmpty(resourceImagePath))
+            {
+                resourceImagePath = testImage;
+            }
+
+            Image = ImageSource.FromResource(resourceImagePath);
+        }
+    }
+}
