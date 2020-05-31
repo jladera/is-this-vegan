7e71dcad82d9a49961013f48cd51c8ce07e53bf2
jladera <38166729+jladera@users.noreply.github.com>
2020-05-30 20:13:12 -0700

    Created result page (not fully implemented)

--- Is This Vegan/Is This Vegan/Is This Vegan/Views/Result.xaml.cs
+++ Is This Vegan/Is This Vegan/Is This Vegan/Views/Result.xaml.cs
@@ -1,21 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Is_This_Vegan.Backend.Tesseract;
+using SkiaSharp;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using Tesseract;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Is_This_Vegan.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class Result : ContentPage
-{
-    public Result(System.Drawing.Image image)
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Result : ContentPage
     {
-        InitializeComponent();
+        private TextExtractor textExtractor;
+
+        public Result(ImageSource source, SKBitmap croppedBitmap)
+        {
+            InitializeComponent();
+            textExtractor = new TextExtractor();
+            var response = textExtractor.ExtractFromImageTestAsync();
+            MyImage.Source = source;
+            
+        }
     }
-}
 }
