853884a4b3fdd304d05159b19fe5ee7251e15e92
jladera <38166729+jladera@users.noreply.github.com>
2020-06-10 22:54:52 -0700

    Removed web api post request call

--- Is This Vegan/Is This Vegan/Is This Vegan/Views/Result.xaml.cs
+++ Is This Vegan/Is This Vegan/Is This Vegan/Views/Result.xaml.cs
@@ -4,21 +4,42 @@ using Tesseract;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Is_This_Vegan.Backend.API;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Is_This_Vegan.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Result : ContentPage
     {
-        private TextExtractor textExtractor;
+        // Instance that connects to RESTful API
+        private TextExtractor textExtractor = new TextExtractor();
 
         public Result(ImageSource source, SKBitmap croppedBitmap)
         {
             InitializeComponent();
-            textExtractor = new TextExtractor();
-            //var response = textExtractor.ExtractFromImageTestAsync();
-            MyImage.Source = source;
-            
+
+            TestPostImageAsync();
+        }
+        public async Task<bool> TestPostImageAsync()
+        {
+            try
+            {
+                var result = await textExtractor.PostImageAsync();
+
+                if (result)
+                {
+                    var displayResult = textExtractor.response;
+                    Console.WriteLine(displayResult);
+                }
+                return true;
+            }
+            catch (HttpRequestException)
+            {
+                await DisplayAlert("Server Issue", "We're sorry, there was an issue contacting the application server. Please try again after waiting a minute.", "Okay");
+            }
+            return false;
         }
     }
 }
