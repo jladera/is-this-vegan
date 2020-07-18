b644bf528feefc025346189badfb20ed4ad2b99d
jladera <38166729+jladera@users.noreply.github.com>
2020-07-18 12:06:10 -0700

    Create EnumExtensions.cs

---
+++ Is This Vegan - API/Is This Vegan (Net)/Enums/EnumExtensions.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by Jake Ladera
+ * 
+ * Intended Use:
+ *      Extension methods for project's Enums
+ */
+
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Is_This_Vegan__Net_.Enums
+{
+    public static class EnumExtentions
+    {
+        /// <summary>
+        /// Gets the Description of an Enum type
+        /// </summary>
+        /// <param name="value"> The Enum's numerical value </param>
+        /// <returns> The Enum's description presented as a string </returns>
+        public static string GetDescription(this Enum value)
+        {
+            return ((DescriptionAttribute)Attribute.GetCustomAttribute(
+                value.GetType().GetFields(BindingFlags.Public | BindingFlags.Static)
+                    .Single(x => x.GetValue(null).Equals(value)),
+                typeof(DescriptionAttribute)))?.Description ?? value.ToString();
+        }
+    }
+}
