a78818dc31f75d984fe5c85699c550f6fb0e13bb
jladera <38166729+jladera@users.noreply.github.com>
2020-08-13 11:22:32 -0700

    Update IngredientListBackend.cs
    Adjusted class to be more testable and removed refrerences to mediaPath.

--- Is This Vegan - API/Is This Vegan (Net)/Backend/Ingredient List/IngredientListBackend.cs
+++ Is This Vegan - API/Is This Vegan (Net)/Backend/Ingredient List/IngredientListBackend.cs
@@ -12,22 +12,22 @@ namespace Is_This_Vegan__Net_.Backend.Ingredient_List
     public class IngredientListBackend
     {
         private string tessdataPath;
-        private string mediaPath;
-        public IngredientListModel list { get; private set; }
-        public ExtractionModel extraction { get; private set; }
-        public Exception exception { get; private set; }
+        public IngredientListModel list { get; set; }
+        public IngredientListHelper helper { get; set; }
+        public ExtractionModel extraction { get; set; }
+        public Exception exception { get; set; }
 
-        public IngredientListBackend(string tessdataPath, string mediaPath)
+        public IngredientListBackend(string tessdataPath)
         {
             this.tessdataPath = tessdataPath;
-            this.mediaPath = mediaPath;
+            helper = new IngredientListHelper();
         }
 
         public bool ExtractFromImage(IngredientListModel list)
         {
             this.list = list;
 
-            var listImage = IngredientListHelper.StringToBitmap(list.imageAsString);
+            var listImage = helper.StringToBitmap(list.imageAsString);
 
             var result = ExtractFromImageTest(listImage);
 
@@ -40,12 +40,6 @@ namespace Is_This_Vegan__Net_.Backend.Ingredient_List
             {
                 using (var engine = new TesseractEngine(tessdataPath, "eng", EngineMode.Default))
                 {
-
-                    // Extract text from test image
-                    if (image is null)
-                    {
-                        image = new Bitmap(Image.FromFile(mediaPath + "\\test_ingredient_list_cropped.jpg"));
-                    }
 
                     // have to load Pix via a bitmap since Pix doesn't support loading a stream.
                     using (var pix = PixConverter.ToPix(image))
