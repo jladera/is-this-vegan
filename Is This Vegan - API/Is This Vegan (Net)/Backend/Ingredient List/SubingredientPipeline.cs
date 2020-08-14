59241802f82a4436a381db0d1e460ba3777aacb1
jladera <38166729+jladera@users.noreply.github.com>
2020-08-13 21:05:51 -0700

    Create SubingredientPipeline.cs
    Implements IPipeline

---
+++ Is This Vegan - API/Is This Vegan (Net)/Backend/Ingredient List/SubingredientPipeline.cs
@@ -0,0 +1,44 @@
+﻿using Is_This_Vegan__Net_.Backend.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Is_This_Vegan__Net_.Backend.Ingredient_List
+{
+    /// <summary>
+    /// Replaces ingredients that have sub-ingredients with their sub-ingredients.
+    /// </summary>
+    public class SubingredientPipeline : IPipeline
+    {
+        /// <summary>
+        /// Pipeline driver
+        /// 
+        /// example input:
+        /// ENRICHED FLOUR [WHEAT FLOUR, NIACIN, REDUCED IRON, THIAMIN MONONITRATE (VITAMIN B1), RIBOFLAVIN (VITAMIN B2), FOLIC ACID]
+        /// 
+        /// example output:
+        /// [WHEAT FLOUR, NIACIN, REDUCED IRON, THIAMIN MONONITRATE (VITAMIN B1), RIBOFLAVIN (VITAMIN B2), FOLIC ACID]
+        /// </summary>
+        /// <param name="input"> Raw ingredient list </param>
+        /// <returns> True if executes without error, otherise false </returns>
+        public bool Execute<T>(ref T input)
+        {
+            //var ingredientsWithSubingredients = FindIngredientsWithSubingredients(input);
+            return true;
+        }
+
+        /// <summary>
+        /// Finds all ingredients that have sub-ingredients
+        /// </summary>
+        /// <param name="input"> Raw ingredient list </param>
+        /// <returns> Ingredients that have sub-ingredients </returns>
+        public MatchCollection FindIngredientsWithSubingredients(string input)
+        {
+            var regex = new Regex(@"(?<=\s)(\w|\s)*(\[|\{)(\w*|\s|\,|\(|\))*(\]|\})");
+            MatchCollection matches = regex.Matches(input);
+            return matches;
+        }
+    }
+}
