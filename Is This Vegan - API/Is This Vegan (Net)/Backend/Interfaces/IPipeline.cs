3260efa5be2fa1276321dec848f2f853eba81d96
jladera <38166729+jladera@users.noreply.github.com>
2020-08-13 21:05:17 -0700

    Create IPipeline.cs

---
+++ Is This Vegan - API/Is This Vegan (Net)/Backend/Interfaces/IPipeline.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_This_Vegan__Net_.Backend.Interfaces
+{
+    interface IPipeline
+    {
+        bool Execute<T>(ref T input);
+    }
+}
