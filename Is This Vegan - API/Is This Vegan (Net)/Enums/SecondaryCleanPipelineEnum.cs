02ac3422b1997ca8a86ded0ede4aa1b10ca18450
jladera <38166729+jladera@users.noreply.github.com>
2020-09-15 13:01:59 -0700

    Create SecondaryCleanPipelineEnum.cs
    Currently only being used for dual-named ingredients (ex. Thiamin Mononitrate (Vitamin B1)) and intermediately-comma'd ingredients (ex. 1,2-Hexanediol), however may be more useful later

---
+++ Is This Vegan - API/Is This Vegan (Net)/Enums/SecondaryCleanPipelineEnum.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by Jake Ladera
+ * 
+ * Intended Use:
+ *      Represent types of ingredients that will be saved and reinserted in the 
+ *      last step of the SecondaryCleanPipeline.
+ */
+
+using System.ComponentModel;
+
+namespace Is_This_Vegan__Net_.Enums
+{
+    public enum SecondaryCleanPipelineEnum
+    {
+        /// <summary>
+        /// Ingredients with a common and scientific name listed
+        /// 
+        /// ex. Thiamin Mononitrate (Vitamin B1)
+        /// ex. Vitamin B1 (Thiamin Mononitrate)
+        /// </summary>
+        TwoNameIngredient = 0,
+
+        /// <summary>
+        /// Ingredients with a comma within its name
+        /// 
+        /// ex. 1,2-Hexanediol
+        /// </summary>
+        IntermediateCommaIngredient = 1
+    }
+}
