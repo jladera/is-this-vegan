e0407b8e05290c800c32938bca15e441e8988cc9
Jake Ladera <laderah@seattleu.edu>
2020-11-10 16:44:31 -0800

    Refactor code
    Removes obselete code that used Regex to replace ingredients with
    subingredients. In its place the class uses a custom algorithm that
    uses opening/closing brackets as an indicator of subingredients.

--- Is This Vegan - API/Is This Vegan (Net)/Backend/Ingredient List/SecondaryCleanPipeline.cs
+++ Is This Vegan - API/Is This Vegan (Net)/Backend/Ingredient List/SecondaryCleanPipeline.cs
@@ -161,55 +161,6 @@ namespace Is_This_Vegan__Net_.Backend.Ingredient_List
         }
 
         /// <summary>
-        /// Finds all ingredients that have sub-ingredients
-        /// </summary>
-        /// <param name="input"> Raw ingredient list </param>
-        /// <returns> Ingredients that have sub-ingredients </returns>
-        public MatchCollection FindSubingredients(string input)
-        {
-            var matches = Regex.Matches(input, @"(?<=\s)(\w|\s)*(\[|\{|\()(\w*|\s|\,)*(\]|\}|\))(?=,)");
-            return matches;
-        }
-
-        /// <summary>
-        /// Finds and returns all lists of subingredients.
-        /// 
-        /// example:
-        ///     extracts: WHEAT FLOUR, NIACIN, REDUCED IRON, THIAMIN MONONITRATE (VITAMIN B1), RIBOFLAVIN (VITAMIN B2), FOLIC ACID
-        ///     from: ENRICHED FLOUR [WHEAT FLOUR, NIACIN, REDUCED IRON, THIAMIN MONONITRATE (VITAMIN B1), RIBOFLAVIN (VITAMIN B2), FOLIC ACID}
-        /// </summary>
-        /// <param name="input"> Raw ingredient list</param>
-        /// <returns></returns>
-        public MatchCollection ExtractSubingredients(string input)
-        {
-            var subingredients = Regex.Matches(input, @"(?<=\[|\{|\()(\w*|\s|\,)*(?=\}|\]|\))");
-            return subingredients;
-        }
-
-        /// <summary>
-        /// Replaces ingredients that contain subingredients with only the subingredients.
-        /// 
-        /// example:
-        ///     replaces: ENRICHED FLOUR [WHEAT FLOUR, NIACIN, REDUCED IRON, THIAMIN MONONITRATE (VITAMIN B1), RIBOFLAVIN (VITAMIN B2), FOLIC ACID}
-        ///     with: WHEAT FLOUR, NIACIN, REDUCED IRON, THIAMIN MONONITRATE (VITAMIN B1), RIBOFLAVIN (VITAMIN B2), FOLIC ACID
-        /// </summary>
-        /// <param name="rawList"> Raw ingredient list </param>
-        /// <param name="toReplace"> List of ingredient sections to replace (return result from Find method) </param>
-        /// <param name="subingredients"> List of subingredients (return result from ExtractSubingredientsMethod) </param>
-        /// <returns> The full ingredient list where ingredients that contain subingredients have been replaced </returns>
-        public string ReplaceParentIngredient(string rawList, MatchCollection toReplace, MatchCollection subingredients)
-        {
-            string result = rawList;
-
-            foreach(var index in Enumerable.Range(0, toReplace.Count))
-            {
-                result = result.Replace(toReplace[index].Value, subingredients[index].Value);
-            }
-
-            return result;
-        }
-
-        /// <summary>
         /// Finds all ingredients that have both a common and scientific name listed, removes them from the list,
         /// and saves the matches to the class variable: DualNamedIngredients
         /// Example Match: "Salt (Sodium Chloride)"
