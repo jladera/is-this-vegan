8a4146a5611768a86c180e7313660a9c3836fa0a
Jake Ladera <laderah@seattleu.edu>
2020-11-17 20:12:11 -0800

    Create Post api for user-authenticated list
    Provides the endpoint that receives a user-authenticated ingredient list
    for a product. Once validated, this list is given to CategorizeBackend
    which will check each ingredient to the local ingredient dictionary,
    thereby enabling the system to categorize the product. This result is
    then returned to the end-user.

---
+++ Is This Vegan - API/Is This Vegan (Net)/Controllers/CategorizeController.cs
@@ -0,0 +1,41 @@
+﻿using Is_This_Vegan__Net_.Backend.Categorize;
+using Is_This_Vegan__Net_.Models;
+using System.Web.Http;
+
+namespace Is_This_Vegan__Net_.Controllers
+{
+    public class CategorizeController : ApiController
+    {
+        public string mediaPath { get; private set; }
+        private CategorizeBackend backend;
+
+        public CategorizeController()
+        {
+            mediaPath = System.Web.HttpContext.Current.Request.MapPath("~\\Media");
+            backend = new CategorizeBackend(mediaPath);
+        }
+
+        public IHttpActionResult Post([FromBody] string ingredientsList)
+        {
+            if (string.IsNullOrEmpty(ingredientsList) ||
+                string.IsNullOrWhiteSpace(ingredientsList))
+            {
+                return BadRequest("Post body cannot be null. Please include an ingredients list as a string and try again.");
+            }
+
+            ProductModel result = backend.CategorizeProduct(ingredientsList);
+
+            if (result is null)
+            {
+                return BadRequest();
+            }
+
+            if (result.IsSuccessful is false)
+            {
+                return InternalServerError();
+            }
+
+            return Ok(result);
+        }
+    }
+}
