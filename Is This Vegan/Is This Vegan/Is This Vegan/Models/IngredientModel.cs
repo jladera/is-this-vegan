e63c18c83e9e85bf0bd17969a1e16f89d620f894
Jake Ladera <laderah@seattleu.edu>
2020-12-01 16:36:25 -0800

    Add IngredientModel to models
    This model aligns with the model used on the web server to represent the
    attributes that belong to a single ingredient.

---
+++ Is This Vegan/Is This Vegan/Is This Vegan/Models/IngredientModel.cs
@@ -0,0 +1,40 @@
+﻿using Is_This_Vegan.Enums;
+using System;
+
+namespace Is_This_Vegan.Models
+{
+    public class IngredientModel
+    {
+        /// <summary>
+        /// GUID as it exists in database
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary> id, name, vegan_status, is_core, last accessed, is debated
+        /// Name of the ingredient
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Classification of the ingredient as Vegan, Not Vegan, Maybe Vegan, Not Recognized 
+        /// </summary>
+        public IngredientClassificationEnum Classification { get; set; }
+
+        /// <summary>
+        /// If ingredient is an ingredient used to load the database initially, it will not
+        /// deleted even if it hasn't been accessed in a long time.
+        /// </summary>
+        public bool IsCore { get; set; }
+
+        /// <summary>
+        /// Date of the last read from the database
+        /// </summary>
+        public DateTime LastAccessed { get; set; }
+
+        /// <summary>
+        /// True if the classification of the ingredient is currently in debate,
+        /// otherwise false.
+        /// </summary>
+        public bool IsDebated { get; set; }
+    }
+}
