d9c38e9a9751dd7ac6f0df87724613417abb07e0
Jake Ladera <laderah@seattleu.edu>
2020-12-15 14:44:30 -0800

    Add ProductModel to models
    This model will be received in the response when posting to the
    Categorize api route, and will be used when displaying the vegan status
    of a scanned product to the user.

--- Is This Vegan/Is This Vegan/Is This Vegan/Models/ProductModel.cs
+++ Is This Vegan/Is This Vegan/Is This Vegan/Models/ProductModel.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Is_This_Vegan.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
-namespace Is This Vegan.Models
+namespace Is_This_Vegan.Models
 {
-    class ProductModel
-{
-}
+    public class ProductModel
+    {
+        /// <summary>
+        /// Indicates whether product categorization is successful
+        /// </summary>
+        public bool IsSuccessful { get; set; }
+
+        /// <summary>
+        /// Indicate how vegan a product is
+        /// </summary>
+        [DisplayName("Category")]
+        public IngredientClassificationEnum ProductCategory { get; set; }
+
+        /// <summary>
+        /// List of ingredients within the product sorted in alphabetical order
+        /// </summary>
+        [DisplayName("Ingredients")]
+        public List<IngredientModel> Ingredients { get; set; }
+    }
 }
